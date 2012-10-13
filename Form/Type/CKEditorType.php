dd19472b411e3e9159012703ff2ba83cd077ba1b
Milos Milovanovic <milos.silni@gmail.com>
2012-10-13 18:21:37 +0200

    init commit

---
+++ Form/Type/CKEditorType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MilosSilni\CKEditorBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+
+class CKEditorType extends AbstractType
+{
+  public function buildForm(FormBuilder $builder, array $options)
+  {
+    $builder->setAttribute('options', $options['options']);
+  }
+  
+  public function buildView(FormView $view, FormInterface $form)
+  {
+    $view->set('options', $form->getAttribute('options'));
+  }
+  
+  public function getDefaultOptions(array $options)
+  {
+    return array(
+        'attr' => array('class' => 'editor-html'),
+        'options' => array()
+    );
+  }
+
+  public function getParent(array $options)
+  {
+    return 'textarea';
+  }
+
+  public function getName()
+  {
+    return 'ckeditor';
+  }
+}
---
+++ MilosSilniCKEditorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MilosSilni\CKEditorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MilosSilniCKEditorBundle extends Bundle
+{
+}
